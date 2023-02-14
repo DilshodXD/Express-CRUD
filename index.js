40a04c1dd25274b8cd40b24ee6e166ff3be51359
Dilshod <dilshodxdxd@gmail.com>
2023-02-14 06:22:11 +0500

    Api and Database connect

--- index.js
+++ index.js
@@ -1,49 +1,26 @@
-const express = require('express');
+// bzyn9vzwRiaN3Mas
+const mongoose = require("mongoose");
+const express = require("express");
+const categoriesRoute = require("./routes/categories");
+
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use("/api/categories", categoriesRoute);
+// mongoose.set("useFindAndModify", false);
+mongoose.set('strictQuery', false);
 
+mongoose
+  .connect(
+    "mongodb+srv://Dilshod:bzyn9vzwRiaN3Mas@backend.nf03kr9.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("mongo db connection on database");
+  })
+  .catch((err) => console.log(err));
 
-const categories = [
-  { id: 1, name: 'JavaScript' },
-  { id: 2, name: 'Python' },
-  { id: 3, name: 'Java' },
-]
-
-app.get('/api/categories', (req, res) => {
-  res.send(categories)
-})
-
-app.get('/api/categories/:id', (req, res) => {
-  const category = categories.filter(lesson => lesson.id === +req.params.id)
-  res.send(category)
-})
-
-app.post('/api/categories', (req, res) => {
-  const category = {
-    id: categories.length + 1,
-    name: req.body.name
-  }
-  categories.push(category)
-  res.status(201).send(category)
-})
-
-app.put('/api/categories/:id', (req, res) => {
-  const category = categories.filter(lesson => lesson.id === +req.params.id)
-  console.log(category.name);
-  category[0].name = req.body.name
-  res.send(category).status(201)
-})
-
-app.delete('/api/categories/:id', (req, res) => {
-  const category = categories.filter(lesson => lesson.id === +req.params.id)
-
-  const categoryIndex = categories.indexOf(category)
-  categories.splice(categoryIndex, 1)
-  res.send(category)
-})
-
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
-})
+});
